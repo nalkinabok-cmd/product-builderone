9f93ad43344927645ae573375cbe67dec16d65f6
nalkinabok <nalkinabok@gmail.com>
2026-02-15 19:35:51 +0000

    feat: Add dark/light mode toggle

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 
 const generateBtn = document.getElementById('generate-btn');
 const numberSpans = document.querySelectorAll('.number');
+const toggleSwitch = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.body.classList.add(currentTheme === 'light' ? 'light-mode' : '');
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 generateBtn.addEventListener('click', () => {
     const lottoNumbers = new Set();
