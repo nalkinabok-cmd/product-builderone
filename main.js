0563f86bb9126d0d1a4d965011112ecbbd82fb64
nalkinabok <nalkinabok@gmail.com>
2026-02-15 19:09:59 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+
+const generateBtn = document.getElementById('generate-btn');
+const numberSpans = document.querySelectorAll('.number');
+
+generateBtn.addEventListener('click', () => {
+    const lottoNumbers = new Set();
+    while (lottoNumbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        lottoNumbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(lottoNumbers).sort((a, b) => a - b);
+
+    numberSpans.forEach((span, index) => {
+        span.textContent = sortedNumbers[index];
+    });
+});
